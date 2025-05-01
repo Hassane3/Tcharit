6b00bb7e95877fcd6f03c81a2878261a7050b011
hassane9 <hsne.lamine@gmail.com>
2025-05-01 18:46:21 +0200

    feat:loader

--- client/src/utils/constants/Icons.tsx
+++ client/src/utils/constants/Icons.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { customTheme } from "../../App";
 interface iconProps {
   backgroundColor?: string;
+  width?: number;
+  height?: number;
 }
 
 export const Logo = () => {
@@ -495,8 +497,8 @@ export const Close = (props: iconProps) => {
   const { backgroundColor } = props;
   return (
     <svg
-      width="22"
-      height="22"
+      width="18"
+      height="18"
       viewBox="0 0 17 17"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
@@ -637,11 +639,11 @@ export const SelectedStar = (props: iconProps) => {
   );
 };
 export const Infos = (props: iconProps) => {
-  const { backgroundColor } = props;
+  const { backgroundColor, width, height } = props;
   return (
     <svg
-      width="18"
-      height="18"
+      width={width || 16}
+      height={height || 16}
       viewBox="0 0 18 18"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
